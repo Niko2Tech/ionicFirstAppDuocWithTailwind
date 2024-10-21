e010779199747b560bb3bbf36aa5c6c290948178
Nicolas Jimenez <niko12121212@gmail.com>
2024-10-21 18:32:46 -0300

    Login funcionando con persistencia de datos, home falta agregar modal ingresar viaje por ahora
    cambio en la api key

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,60 +1,210 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+declare var google: any;
+
+interface Viaje {
+  id: number;
+  usuario: string;
+  carrera: string;
+  destino: string;
+  valor: number;
+  fecha: Date;
+  asientosDisponibles: number;
+  asientosOcupados: number;
+  asientosTotales: number;
+  nota: number;
+  idUsuariosIngresados: number[];
+}
+interface Usuario {
+  correo: string;
+  direccion: string;
+  nombre: string;
+  password: string;
+  rut: string;
+  telefono: string;
+  viajes_creados: Viaje[];
+  viajes_solicitados: Viaje[];
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0,
+        transform: 'translateY(-50px)'
+      })),
+      transition(':enter', [
+        animate('300ms ease-in', style({
+          opacity: 1,
+          transform: 'translateY(-10px)'
+        }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({
+          opacity: 0,
+          transform: 'translateY(-50px)'
+        }))
+      ])
+    ])
+  ]
 })
+
+
+
 export class HomePage implements OnInit {
   icono: string = '';
   logoDuoc: string = '';
-  usuario: string = 'Admin';
-  carrera_por_defecto: string = 'ING. Informatica';
-  destino: string = '';
-  valor: number = 0;
-  asientosDisponibles: number = 0;
-  asientosTotales: number = 0;
-  nota: number = 4;
-
-  viajes = [
-    {
-      id: 1,
-      usuario: 'Lucas Flores',
-      carrera: 'ING. Informatica',
-      destino: 'Altos del Maiten',
-      valor: 1500,
-      asientosDisponibles: 3,
-      asientosTotales: 5,
-      nota: 2
-    },
-    {
-      id: 2,
-      usuario: 'Juan Perez',
-      carrera: 'Mecanica',
-      destino: 'Lomas de manso',
-      valor: 1000,
-      asientosDisponibles: 2,
-      asientosTotales: 3,
-      nota: 3.5
-    },
-    {
-      id: 3,
-      usuario: 'Luis Hernandez',
-      carrera: 'ING. Informatica',
-      destino: 'Bollenar',
-      valor: 2000,
-      asientosDisponibles: 4,
-      asientosTotales: 5,
-      nota: 4.5
-    }
-  ];
+  usuario: Usuario | null = null;
+  menuVisible = false;
+  viajes: Viaje[] = [];
   modalAbierto: string | null = null;
   animateClass: string = '';
+  nombreUsuario: string = '';
+
+  @ViewChild('map', { static: false }) mapElement!: ElementRef;
+  public map: any;
+  public directionsService: any;
+  public directionsDisplay: any;
+  public start: any = "Duoc UC: Sede Melipilla - Serrano, Melipilla, Chile";
+  public end: any = "Pomaire";
+  public autocompleteItems: any[] = [];
+  distancia: string = '';
+  duracion: string = '';
   
-  constructor() {}
+  constructor(private navCtrl: NavController, private platform: Platform, private zone: NgZone) {}
+
 
   ngOnInit() {
     this.getTheme();
+    this.getSeccion();
+  }
+
+  getSeccion() {
+    // Obtener la sesión desde LocalStorage
+    const session = JSON.parse(localStorage.getItem('session') || '{}');
+
+    // Verificar si la sesión es válida
+    if (!session || !session.correo || !session.expiration) {
+      this.navCtrl.navigateForward('/login');
+      return;
+    }
+
+    if (session.expiration < new Date().getTime()) {
+      localStorage.removeItem('session');
+      this.navCtrl.navigateForward('/login');
+      return;
+    }
+
+    // Obtener el correo del usuario desde la sesión
+    const correo = session.correo;
+
+    // Obtener la lista de usuarios desde el LocalStorage (base de datos simulada)
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+
+    // Buscar el usuario por correo
+    const usuarioEncontrado = users.find((user: Usuario) => user.correo === correo);
+
+    // Si se encuentra el usuario, lo asignamos a la variable usuario
+    if (usuarioEncontrado) {
+      this.usuario = usuarioEncontrado;
+    } else {
+      console.error('Usuario no encontrado en la base de datos de users');
+    }
+    this.nombreUsuario = this.usuario?.correo?.split('@')[0] || 'Usuario no identificado';
+  }
+
+  initMap() {
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+
+    const mapOptions = {
+      zoom: 5,
+      zoomControl: false,
+      scaleControl: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      fullscreenControl: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.directionsDisplay.setMap(this.map);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          const infoWindow = new google.maps.InfoWindow({
+            position: pos,
+            content: "Estas aquí.",
+          });
+          infoWindow.open(this.map);
+          this.map.setCenter(pos);
+        }
+      );
+    }
+
+    this.calculateAndDisplayRoute();
+  }
+
+  calculateAndDisplayRoute() {
+    this.directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, (response: any, status: string) => {
+      if (status === 'OK') {
+        this.directionsDisplay.setDirections(response);
+        const route = response.routes[0];
+        const leg = route.legs[0];
+
+        const distanceInKilometers = (leg.distance.value / 1000).toFixed(2);
+        this.distancia = `${distanceInKilometers} km`;
+
+        const durationInSeconds = leg.duration.value;
+        const minutes = Math.floor(durationInSeconds / 60);
+        const seconds = durationInSeconds % 60;
+        const formattedDuration = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        this.duracion = `${formattedDuration}`;
+
+        leg.steps.forEach((step: any, index: number) => {
+          console.log(`Paso ${index + 1}: ${step.instructions}, Distancia: ${(step.distance.value / 1000).toFixed(2)} km, Tiempo: ${(step.duration.value / 60).toFixed(2)} minutos`);
+        });
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+  updateSearchResults() {
+    const GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    if (this.end === '') {
+      this.autocompleteItems = [];
+      return;
+    }
+
+    GoogleAutocomplete.getPlacePredictions({ input: this.end }, (predictions: any, status: any) => {
+      this.zone.run(() => {
+        this.autocompleteItems = [];
+        predictions.forEach((prediction: any) => {
+          this.autocompleteItems.push(prediction);
+        });
+      });
+    });
+  }
+
+  selectSearchResult(item: any) {
+    this.end = item.description;
+    this.autocompleteItems = [];
+    this.initMap();
   }
 
   abrirModal(id:string) {
@@ -120,21 +270,32 @@ export class HomePage implements OnInit {
   }
 
   agregarViaje() {
-    const nuevoId = this.viajes.length > 0 ? Math.max(...this.viajes.map(v => v.id)) + 1 : 1;
-
-    const nuevoViaje = {
-      id: nuevoId,
-      usuario: this.usuario,
-      carrera: this.carrera_por_defecto,
-      destino: this.destino,
-      valor: this.valor,
-      asientosDisponibles: this.asientosDisponibles,
-      asientosTotales: this.asientosTotales,
-      nota: this.nota, 
-    };
-
-    this.viajes.push(nuevoViaje);
-    this.cerrarModal();
+    return;
   }
 
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  cerrarSesion() {
+    // borramos la sesión de LocalStorage
+    localStorage.removeItem('session');
+    this.navCtrl.navigateForward('/login');
+    return;
+  }
+
+  mostrarDatos() {
+    this.animateClass = 'animate-slide-in';
+    this.modalAbierto = 'mostrarDatos';
+    this.menuVisible = false;
+  }
+
+  modalIngresarViaje() {
+    this.animateClass = 'animate-slide-in';
+    this.modalAbierto = 'ingresoViaje';
+    this.menuVisible = false;
+    setTimeout(() => {
+      this.initMap();
+    }, 300);
+  }
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -54,11 +54,34 @@ export class LoginPage implements OnInit {
   password_res_error: string = '';
   password_confirm: string = '';
   password_confirm_error: string = '';
+  nombre_res: string = '';
+  rut_res: string = '';
+  telefono_res: string = '';
+  direccion_res: string = '';
+  // errores
+  nombre_res_error: string = '';
+  rut_res_error: string = '';
+  telefono_res_error: string = '';
+  direccion_res_error: string = '';
+  rutRegex: RegExp = /^\d{1,2}\.\d{3}\.\d{3}-[0-9kK]$/;
+  telefonoRegex = /^\+56\d{9}$/;
+  
   
   constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
     this.getTheme();
+    this.getSeccion();
+  }
+
+  getSeccion() {
+    const session = JSON.parse(localStorage.getItem('session') || '{}');
+    if (session && session.expiration > new Date().getTime()) {
+      // Si hay sesión y no ha expirado, redirigimos al home directamente
+      this.navCtrl.navigateForward('/home');
+    } else {
+      localStorage.removeItem('session');
+    }
   }
 
   getTheme() {
@@ -95,30 +118,58 @@ export class LoginPage implements OnInit {
   }
 
   loginButton() {
-    // verificamos el formato del correo 
+    // Limpiamos los errores
+    this.limpiarErrores();
+  
+    // Verificamos el formato del correo 
     if (!this.correo.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
       this.correo_error = 'Formato de correo incorrecto';
-      return
+      return;
     }
-    // verificamos la longitud del correo
-    if (this.correo.length < 5) {
-      this.correo_error = 'Correo muy corto';
-      return
-    }
-    // verificamos la longitud del password
+  
+    // Verificamos la longitud del password
     if (this.password.length < 5) {
-      this.password_error = 'Contraseña muy corta';
-      return
+      this.password_error = 'Contraseña no válida';
+      return;
     }
-    if (this.correo == 'admin@admin.com' && this.password == 'admin') {
+  
+    // Recuperamos los usuarios del LocalStorage
+    let users = JSON.parse(localStorage.getItem('users') || '[]');
+    let user = users.find((u: any) => u.correo === this.correo && u.password === this.password);
+  
+    if (user) {
+      // Guardar la sesión en LocalStorage con tiempo de expiración
+      const expirationTime = new Date().getTime() + 3600000; // 1 hora en milisegundos
+      localStorage.setItem('session', JSON.stringify({
+        correo: this.correo,
+        expiration: expirationTime
+      }));
+  
+      // Navegamos a la página de inicio
       this.navCtrl.navigateForward('/home');
     } else {
       this.error_general = 'Correo o contraseña incorrectos';
     }
-    return
   }
+  
 
   registroButton() {
+    if (this.nombre_res.length < 8) {
+      this.nombre_res_error = 'Nombre y apellido muy corto';
+      return
+    }
+    if (!this.rut_res.match(this.rutRegex)) {
+      this.rut_res_error = 'Formato de rut incorrecto';
+      return
+    }
+    if (!this.telefono_res.match(this.telefonoRegex)) {
+      this.telefono_res_error = 'Formato de telefono incorrecto';
+      return
+    }
+    if (this.direccion_res.length < 5) {
+      this.direccion_res_error = 'Direccion muy corta';
+      return
+    }
     // verificamos el formato del correo
     if (!this.correo_res.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
       this.correo_res_error = 'Formato de correo incorrecto';
@@ -138,10 +189,28 @@ export class LoginPage implements OnInit {
       this.password_confirm_error = 'Las contraseñas no coinciden';
       return
     }
-    this.navCtrl.navigateForward('/home');
-  }
+    
 
-  iniciarMicrosoft() {
+    // Guardar el usuario en LocalStorage
+    let users = JSON.parse(localStorage.getItem('users') || '[]');
+    users.push({
+      correo: this.correo_res,
+      password: this.password_res,
+      nombre: this.nombre_res,
+      telefono: this.telefono_res,
+      rut: this.rut_res,
+      direccion: this.direccion_res,
+      viajes_creados: [],  // Inicializamos el array vacío
+      viajes_solicitados: []  // Inicializamos el array vacío
+    });
+    localStorage.setItem('users', JSON.stringify(users));
+
+    const expirationTime = new Date().getTime() + 3600000; // 1 hora en milisegundos
+    localStorage.setItem('session', JSON.stringify({
+      correo: this.correo_res,
+      expiration: expirationTime
+    }));
+
     this.navCtrl.navigateForward('/home');
   }
    
@@ -149,7 +218,35 @@ export class LoginPage implements OnInit {
     this.correo_error = '';
     this.password_error = '';
     this.error_general = '';
+    this.nombre_res_error = '';
+    this.rut_res_error = '';
+    this.telefono_res_error = '';
+    this.direccion_res_error = '';
+    this.correo_res_error = '';
+    this.password_res_error = '';
+    this.password_confirm_error = '';
   }
+  formatRut(value: string = ""): string {
+    // Elimina caracteres no válidos, solo deja dígitos y 'K' o 'k'
+    let cleanValue = value.replace(/[^0-9kK]/g, '');
+  
+    // Aplica el formateo del RUT
+    if (cleanValue.length > 7) {
+      return `${cleanValue.slice(0, -7)}.${cleanValue.slice(-7, -4)}.${cleanValue.slice(-4, -1)}-${cleanValue.slice(-1)}`;
+    } else if (cleanValue.length > 4) {
+      return `${cleanValue.slice(0, -4)}.${cleanValue.slice(-4, -1)}-${cleanValue.slice(-1)}`;
+    } else if (cleanValue.length > 1) {
+      return `${cleanValue.slice(0, -1)}-${cleanValue.slice(-1)}`;
+    }
+    
+    return cleanValue;
+  }
+
+  limpiarYFormatearRut() {
+    this.rut_res_error = '';
+    this.rut_res = this.formatRut(this.rut_res);
+  }
+
 
   mostrarRegistro() {
     this.login = false;
